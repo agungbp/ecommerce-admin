75ae3aee7b4aa890cdd38bf3e3009f298f53c118
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-04-23 16:54:42 +0700

    create storeswitcher component (error disabled can't click)

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -22,7 +23,7 @@ export default async function DashboardLayout({ children, params }: { children: 
 
     return (
         <>
-            <div>This will be a Navbar</div>
+            <Navbar />
             {children}
         </>
     )
---
+++ components/navbar.tsx
@@ -0,0 +1,34 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "./main-nav";
+import StoreSwitcher from "./store-switcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async () => {
+    const { userId } = auth()
+
+    if (!userId) {
+        redirect('/sign-in')
+    }
+
+    const store = await prismadb.store.findMany({
+        where: {
+            userId
+        }
+    })
+
+    return (
+        <div className="border-b">
+            <div className="flex items-center h-16 px-4">
+                <StoreSwitcher items={store} />
+                <MainNav className="mx-6" />
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
