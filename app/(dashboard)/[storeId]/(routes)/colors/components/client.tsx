2b5ff7848d865744ed1a3c3f60b6bba3c8da1ec7
Agung Budie Pangestu <agungbudiepangestu@gmail.com>
2024-04-30 16:43:03 +0700

    create color page

--- app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -5,15 +5,15 @@ import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
-import { SizeColumn, columns } from "./columns"
+import { ColorColumn, columns } from "./columns"
 import { DataTable } from "@/components/ui/data-table"
 import { ApiList } from "@/components/ui/api-list"
 
-interface SizesClientProps {
-    data: SizeColumn[]
+interface ColorsClientProps {
+    data: ColorColumn[]
 }
 
-export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+export const ColorsClient: React.FC<ColorsClientProps> = ({ data }) => {
     const router = useRouter()
     const params = useParams()
 
@@ -21,19 +21,19 @@ export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={`Sizes (${data.length})`}
-                    description="Manage sizes for yuour store"
+                    title={`Colors (${data.length})`}
+                    description="Manage colors for yuour store"
                 />
-                <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
                     <Plus className="w-4 h-4 mr-2" />
                     Add New
                 </Button>
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="name" />
-            <Heading title="API" description="API calls for sizes" />
+            <Heading title="API" description="API calls for colors" />
             <Separator />
-            <ApiList entityName="sizes" entityIdName="sizeId" />
+            <ApiList entityName="colors" entityIdName="colorId" />
         </>
     )
 }
